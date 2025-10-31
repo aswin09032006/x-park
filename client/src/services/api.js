53ae037596409a75c9bbf741a7c481f5f506ff99
ASWIN V K <vkaswinkanan@gmail.com>
2025-10-31 20:42:32 +0530

    update: change API URL to the correct secure backend endpoint

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://xpark-backend-env.eba-vjbjyfhi.us-east-1.elasticbeanstalk.com/api';
+const API_URL = 'https://api-xpark.creozen.co.uk/api';
 
 // --- NEW: A dedicated function for public (unauthenticated) API calls ---
 export const publicApi = async (endpoint, method = 'GET', body = null) => {
